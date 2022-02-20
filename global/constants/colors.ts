133a66ddab4c06757f887c1b2c177d4268dffd46
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-02-20 13:17:46 -0300

    style: :art: create a theme provider and organize directory structure

---
+++ global/constants/colors.ts
@@ -0,0 +1,12 @@
+const Colors = {
+  background: "#F7F7F7",
+  textPrimary: "#707070",
+  textSecondary: "#868686",
+  textTernary: "#C1C1C1",
+  placeholder: "#9D9D9D",
+  numberButtonBackground: "#ADC0C4",
+  greenPrimary: "#B5C401",
+  greenSecondary: "#27C383",
+};
+
+export default Colors;
---
+++ screens/LoginPage/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+import { Title } from "./styles";
+import { Screen } from "../../global/styles";
+
+const LoginPage = () => {
+  return (
+    <Screen>
+      <Title>LoginPage</Title>
+    </Screen>
+  );
+};
+
+export default LoginPage;
