1d813166a20be94236e52b65f93a122b99659349
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-02-21 14:18:23 -0300

    feat: :construction: create the registration page

--- screens/RegistrationPage/index.tsx
+++ screens/RegistrationPage/index.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
-const RegistrationPage = () => {
+import { Screen, PageTitle, ButtonText, AuthButton } from "./styles";
+import { Card } from "global/styles";
+import { Input } from "components";
+
+import { AntDesign } from "@expo/vector-icons";
+
+import Colors from "global/constants/colors";
+
+const RegistrationPage = ({ navigation }: any) => {
   return (
-    <View>
-      <Text>RegistrationPage</Text>
-    </View>
+    <Screen>
+      <PageTitle>Registration</PageTitle>
+      <Card style={styles.boxShadow}>
+        <Input title="Name" />
+        <Input title="Email" />
+        <Input title="Password" />
+        <AuthButton>
+          <ButtonText green={true}> Register</ButtonText>
+          <AntDesign name="arrowright" size={32} color={Colors.greenPrimary} />
+        </AuthButton>
+      </Card>
+      <AuthButton onPress={() => navigation.goBack()}>
+        <AntDesign name="arrowleft" size={32} color={Colors.textPrimary} />
+        <ButtonText>Back</ButtonText>
+      </AuthButton>
+    </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  boxShadow: {
+    elevation: 5,
+  },
+});
 
 export default RegistrationPage;
--- screens/RegistrationPage/styles.ts
+++ screens/RegistrationPage/styles.ts
@@ -1,1 +1,31 @@
 import styled from "styled-components/native";
+
+import { DefaultScreen, Title } from "global/styles";
+import Colors from "global/constants/colors";
+
+export const Screen = styled(DefaultScreen)`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PageTitle = styled(Title)`
+  font-size: 35px;
+  margin-bottom: 33px;
+`;
+
+export const AuthButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  height: 80px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const ButtonText = styled(Title)<{ green?: boolean }>`
+  color: ${(props: any) =>
+    props.green ? Colors.greenPrimary : Colors.textPrimary};
+  font-size: 35px;
+  margin: 0 10px;
+`;
